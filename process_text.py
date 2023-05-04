1131912d6af01820403b79a85f2fe9785abc4aa5
Gabe Maayan <gabemgem@gmail.com>
2023-05-03 22:59:56 -0400

    v0.1.0 Release

--- process_text.py
+++ process_text.py
@@ -1,13 +1,47 @@
 import string
 
-# with open('shakespeare.txt', 'r') as file:
-#     data = file.read().replace('\n', ' ').replace('.', ' ')
-#     data = data.translate(str.maketrans('','',string.punctuation))
-#     with open('temp.txt', 'w') as output:
-#         output.write(data)
+def remove_punc_and_newlines():
+    with open('shakespeare.txt', 'r') as file:
+        data = file.read().replace('\n', ' ').replace('.', ' ')
+        data = data.translate(str.maketrans('','',string.punctuation))
+        with open('temp.txt', 'w') as output:
+            output.write(data)
 
-with open('temp.txt', 'r') as file:
-    data = file.read()
-    data = ','.join(' '.join(data.split()).split(' '))
-    with open('shakespeare.csv', 'w') as output:
-        output.write(data)
+def make_into_csv():
+    with open('temp.txt', 'r') as file:
+        data = file.read()
+        data = ','.join(' '.join(data.split()).split(' '))
+        with open('shakespeare.csv', 'w') as output:
+            output.write(data)
+
+def make_larger_same_card(multiply:int):
+    with open('shakespeare.csv') as file:
+        data = file.read()
+        with open(f'shakespeare_x_{multiply}.csv', 'w') as output:
+            output.write(data)
+            for i in range(1, multiply):
+                output.write(',')
+                output.write(data)
+
+def make_larger_new_card(multiply:int):
+    with open('shakespeare.csv') as file:
+        data = file.read()
+        with open(f'shakespeare_x_{multiply}_new_card.csv', 'w') as output:
+            output.write(data)
+            split_data = data.split(',')
+            for i in range(1, multiply):
+                temp = ','.join([v+str(i) for v in split_data])
+                output.write(',')
+                output.write(temp)
+
+make_larger_same_card(5)
+make_larger_new_card(5)
+
+make_larger_same_card(10)
+make_larger_new_card(10)
+
+make_larger_same_card(50)
+make_larger_new_card(50)
+
+make_larger_same_card(100)
+make_larger_new_card(100)
--- pycudahll/StandardHLL.py
+++ pycudahll/StandardHLL.py
@@ -1,16 +1,12 @@
-import hyperloglog
 
-hll = hyperloglog.HyperLogLog(0.01) # accept 1% counting error
-print(hll.m, hll.p)
-
-# with open('shakespeare.csv', 'r') as file:
-#     data = file.read().split(',')
-#     for val in data:
-#         hll.add(val)
-
-#     print(len(hll))
-
-def getCPUHLLCardinality(data):
+def addDataCPUHLL(chll, data):
     for val in data:
-        hll.add(val)
-    return len(hll)
+        chll.add(val)
+
+def getCardCPUHLL(chll):
+    return len(chll)
+
+
+def getCPUHLLCardinality(chll, data):
+    addDataCPUHLL(chll, data)
+    return getCardCPUHLL(chll)
